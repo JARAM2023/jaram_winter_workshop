02b90ec0f211b696cbf055958e0c0209b1ecb151
K1A2 <jckim0307@gmail.com>
2023-02-05 08:06:25 +0900

    [FEAT] django 탬플릿 vue.js cdn 적용

--- board/views.py
+++ board/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    print(11)
+    return render(request, 'index.html')
---
+++ templates/masterpage.html
@@ -0,0 +1,1 @@
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
