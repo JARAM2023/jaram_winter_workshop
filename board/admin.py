1d5b7ccba9eeae8f4ec0000902a236dffbfd0917
K1A2 <jckim0307@gmail.com>
2023-02-05 08:37:05 +0900

    [FEAT] 모델 admin 페이지 생성 #1

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Team,
+    SubmitResult,
+    LeaderBoard
+)
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(SubmitResult)
+admin.site.register(LeaderBoard)
