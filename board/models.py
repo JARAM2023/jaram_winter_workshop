3f7e31c1d4df9699a65204d3db0dbd6ba1e50d93
K1A2 <jckim0307@gmail.com>
2023-02-05 08:24:57 +0900

    [FEAT] 모델 마이그레이션 #1

--- board/models.py
+++ board/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Team(models.Model):
+    team_pk = models.AutoField(primary_key=True)
+    team_name = models.CharField(max_length=20)
+    team_member = models.ManyToManyField(User, related_name='team_user')
+
+class SubmitResult(models.Model):
+    submit_pk = models.AutoField(primary_key=True)
+    submit_file = models.FileField(upload_to="submits/")
+    submit_team_pk = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="submit_team")
+    submit_score = models.DecimalField(decimal_places=4, max_digits=6)
+    submit_create = models.DateTimeField(auto_now_add=True)
+
+class LeaderBoard(models.Model):
+    leader_pk = models.AutoField(primary_key=True)
+    leader_submit_pk = models.OneToOneField(SubmitResult, related_name='leader_submit', on_delete=models.CASCADE)
