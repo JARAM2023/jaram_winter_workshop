a9f120a7935b6a528b4a946f3ef1bc7d3e350e3f
K1A2 <jckim0307@gmail.com>
2023-02-05 14:20:06 +0900

    [FEAT] 로그인 구현 #2

--- board/urls.py
+++ board/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from .views import (
     page_index,
     page_login,
-    form_login
+    form_login,
+    page_logout
 )
 
 urlpatterns = [
     path('', page_index, name='index'),
     path('login/', page_login, name='login'),
     path("login/process/", form_login, name="form_login"),
+    path("logout/process/", page_logout, name="logout"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponseBadRequest
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def page_index(request):
     if request.user.is_anonymous:
@@ -9,10 +9,33 @@ def page_index(request):
         return render(request, 'index.html')
 
 def page_login(request):
-    return render(request, 'login.html')
+    if request.user.is_anonymous:
+        if 'message' in request.session:
+            msg = request.session['message']
+            del request.session['message']
+            return render(request, 'login.html', {'message': msg})
+        return render(request, 'login.html')
+    else:
+        return redirect('index')
+
+def page_logout(request):
+    if request.user.is_anonymous:
+        return redirect('login')
+    logout(request)
+    print(request.session)
+    return redirect('login')
 
 def form_login(request):
     if request.method == 'POST':
-        id = request.POST['']
+        id = request.POST['text_id']
+        password = request.POST['text_password']
+        user = authenticate(request, username=id, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            request.session['message'] = '유저가 존재하지 않거나 패스워드가 틀렸습니다.'
+            return redirect('login')
     else:
-        return render(request, 'login.html', {'message': 'request error'})
+        request.session['message'] = 'request error.'
+        return redirect('login')
