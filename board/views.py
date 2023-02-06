ff499e2fa3f39b2a5cd8a5700b6d70e16133beac
K1A2 <jckim0307@gmail.com>
2023-02-06 20:30:11 +0900

    [FEAT] 데이터 설명 구현 #3

--- board/views.py
+++ board/views.py
@@ -12,10 +12,13 @@ def page_index(request):
         return redirect('login')
     else:
         index_cont = Explain.objects.get(explain_id='index_cont').explain_text
+        index_data = Explain.objects.get(explain_id='index_data').explain_text
         index_cont = markdown.markdown(index_cont)
+        index_data = markdown.markdown(index_data)
         print(index_cont)
         return render(request, 'index.html', {
-            'index_cont': index_cont
+            'index_cont': index_cont,
+            'index_data': index_data
         })
 
 def page_login(request):
